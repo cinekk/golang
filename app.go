991bdc39174de878a7740e67e6f0d4adf7a30ec6
Tomasz <trzcinski.t@gmail.com>
2018-10-18 15:58:32 +0200

    Number guessing game

--- app.go
+++ app.go
@@ -1,13 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"math/rand"
+	"time"
+	"strconv"
+)
 
-func Sum(x, y int) (sum int) {
-	sum = x + y
-	return
+func intro() {
+	fmt.Println("ELO bijacz. Welkum to da game!")
+	fmt.Println("System will generate a random number in rage 1 - 100")
+	fmt.Println("You need to guess dat.")
+	prompt()
+}
+
+func prompt() {
+	fmt.Println("")
+	fmt.Println("Type in yer guess:")
 }
 
 func main() {
-	fmt.Println(Sum(10, 3))
-	fmt.Println("Hello Dupa")
+	scanner := bufio.NewScanner(os.Stdin)
+	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	randNum := rand.Intn(100)
+
+	intro()
+
+	for scanner.Scan() {
+		
+		guess := scanner.Text()
+
+		var guessed int
+
+		guessed, err := strconv.Atoi(guess)
+
+		if err != nil {
+			fmt.Println("NO INPUT BIJACZ")
+		}
+
+		var msg string
+
+		if guessed != randNum {
+			if guessed > randNum {
+				msg = "too high"
+			} else {
+				msg = "too low"
+			}
+			fmt.Println("Guess is no good! Guesss", msg, "!!!")
+
+			prompt()
+		} else {
+			fmt.Println("GUESSS OKAYYYY")
+			os.Exit(0)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+        fmt.Fprintln(os.Stderr, "error:", err)
+        os.Exit(1)
+    }	
 }
